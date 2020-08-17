981f0c7b8194cc8570b6410c6123463e9b486780
thejames <james@zambon.us>
2020-08-17 00:29:06 -0400

    Initial commit

---
+++ data.php
@@ -0,0 +1,194 @@
+<?php
+# Lets load some details!!
+$general        =   array(
+    Author      =>  "James Zambon",
+    Subtitle    =>  "Flight Paramedic, Photographer, Programmer",
+    Tagline     =>  "I am a Flight Paramedic with 19+ years experience primarily in Critical Care and the most recent 5 years in both Fixed Wing / Air Ambulance and Commercial Medical Escort work. I have been instructors in ACLS, BLS",
+
+    DOB         =>  "12 July 1974",
+    Address     =>  "500 Trinity Ln N #12206, St Pete, FL",
+    Phone       =>  "727.430.9583",
+    Email       =>  "&#074;&#097;&#109;&#101;&#115;&#064;&#090;&#097;&#109;&#098;&#111;&#110;&#046;&#117;&#115;",
+    
+    Facebook    =>  "jazambon",
+    Twitter     =>  "_thejames",
+    LinkedIn    =>  "jazambon",
+    Flickr      =>  "jameszambon",
+    MailLink    =>  $locators[ Email ]
+);
+
+$jobs   =   array(
+  array(
+    Title       =>  "Flight Paramedic",
+    Employer    =>  "Commercial Medical Escort: Private Contractor",
+    StartDate   =>  "2008",
+    EndDate     =>  " +",
+    
+    Description =>  "Contractor to: <a href=\"http://www.parkviewairmedical.com\">Parkview Airmedical</a>, <a href=\"https://www.linkedin.com/pub/imc-meditrans-katie-byrne/6a/99/837\">IMC Meditrans</a>, and <a href=\"http://www.globalmedservices.com\">Global Med Services</a><br>
+        Approximately 55 transports / year (450-500k miles) for patients of varying severity.",
+  ),
+  array(
+    Title       =>  "Flight Paramedic",
+    Employer    =>  "Air Ambulance: Private Contractor",
+    StartDate   =>  "2008",
+    EndDate     =>  " +",
+
+    Description =>  "Contractor to: <a href=\"http://globaljetcare.com\">Global Jet Care</a></p>
+        <p>International long-haul transports of primarily ICU and CCU patients of varying severities.",
+  ),
+  array(
+    Title       =>  "AHA ACLS / BLS Instructor",
+    Employer    =>  "MATS / Tampa General &amp; USF College of Medicine",
+    StartDate   =>  "2009",
+    EndDate     =>  " &ndash; 14",
+    
+    Description =>  "Instructing USF medical and nursing school students and Tampa General Hospital Staff in AHA BLS and ACLS courses.",
+  ),
+  array(
+    Title       =>  "Web Developer",
+    Employer    =>  "MityMo Creative, Part-Time",
+    StartDate   =>  "2009",
+    EndDate     =>  " &ndash; 11",
+
+    Description =>  "Build fancy things for websites using Ruby, Ruby on Rails, PHP, Pearl, Javascript, HTML5, CSS and other web technologies as required."
+  ),
+  array(
+    Title       =>  "Programmer / Web Developer",
+    Employer    =>  "Self Employed",
+    StartDate   =>  "2006",
+    EndDate     =>  " +",
+    
+    Description =>  "Build fancy things for websites using Ruby, Ruby on Rails, PHP, Pearl, Javascript, HTML5, CSS and other web technologies as required."
+  ),
+  array(
+    Title       =>  "Event and Portraiture Photography",
+    Employer    =>  "Self Employed",
+    StartDate   =>  "2006",
+    EndDate     =>  " +",
+
+    Description =>  "Journalistic style approach to document weddings, conferences and other events Portraits for model portfolios, media packet information for dramatic artists, etc.<br>
+      Produced several local gallery showings of my photography at Cafe Bohemia, Studio at 620, and The Vitale Gallery.",
+  ),
+  array(
+    Title       =>  "Critical Care Paramedic",
+    Employer    =>  "Sunstar Paramedics, Full-Time",
+    StartDate   =>  "1998",
+    EndDate     =>  " &ndash; 08",
+    
+    Description =>  "<p>Operations Supervisor,
+        NR-CCEMT-Paramedic<br>
+        NAEMD certified and Communication Center trained, System Status Controller<br>
+        Preceptor for new Paramedics<br>
+        Instructor for St Petersburg College EMT and Paramedic students, and the Explorer program<br>
+        All Children&lsquo;s Transport Team Paramedic<br>
+        Represented Sunstar at <a href=\"http://www.emlrc.org\">Clin-Con</a> as team captain<br>
+        Participated in <abbr title=\"Total Quality Management\">TQM</abbr> teams<br>
+        Developed and implemented a training and certification program <br>
+        Developed and implemented a supervision training system Developed an improved method to select, promote and train Assistant Supervisors"
+  ),
+  array(
+    Title       =>  "Medical Control Officer",
+    Employer    =>  "Office of the Medical Director; Largo, FL, Part-Time",
+    StartDate   =>  "1999",
+    EndDate     =>  " &ndash; 02",
+
+    Description =>  "Assisted with on-line medical direction of on duty Paramedics. 
+          Handled documentation of multiple incident registries for continuing quality improvement. 
+          Handled documentation and initial investigation of Quality Assurance Reviews. 
+          Provided ambulance personnel with hospital Radio Channels."
+  )
+  /* array( */
+  /*   Title       =>  "", */
+  /*   Employer    =>  "", */
+  /*   DateRange   =>  "", */
+  /*   Description =>  "" */
+  /* ) */
+);
+
+$educations = array(
+  array(
+    TheSchool   =>  "Excelsior College",
+    Degree      =>  "Bachelor of Science Nursing",
+    DateRange   =>  "In process"
+  ),
+  array(
+    TheSchool   =>  "St. Petersburg College, FL",
+    Degree      =>  "EMT-Paramedic",
+    DateRange   =>  "1993 &ndash; 96"
+  )
+  /* array( */
+  /*   TheSchool   =>  "", */
+  /*   Degree      =>  "", */
+  /*   DateRange   =>  "" */
+  /* ) */
+);
+
+$certifications = array(
+  array(
+    Title       =>  "Paramedic (FL)",
+    Expiration  =>  "12/2016",
+  ),
+  array(
+    Title       =>  "DOT Flight Certification",
+    Expiration  =>  "N/A",
+  ),
+  array(
+    Title       =>  "AHA ACLS",
+    Expiration  =>  "06/2017",
+  ),
+  array(
+    Title       =>  "AHA BLS",
+    Expiration  =>  "09/2016",
+  )
+);
+
+$Languages = array(
+  array(
+    Name        =>  "Italian",
+    Expertise   =>  "Good"
+  ),
+  array(
+    Name        =>  "French",
+    Expertise   =>  "Very Basic"
+  ),
+  array(
+    Name        =>  "Spanish",
+    Expertise   =>  "Very Basic"
+  )
+);
+
+$Visas = array(
+  array(
+    Country     =>  "Brazil",
+    Expiration  =>  "December 2020"
+  ),
+  array(
+    Country     =>  "Nexus",
+    Expiration  =>  "July 2017"
+  ),
+  array(
+    Country     =>  "Global Enrty",
+    Expiration  =>  "July 2017"
+  ),
+  array(
+    Country     =>  "EU Passport (Italy)",
+    Expiration  =>  "Pending Document approval"
+  )
+);
+
+$miscellaneous = array(
+  array(
+    Name        =>  "Delta",
+    Description =>  "360 / Diamond Medallion"
+  ),
+  array(
+    Name        =>  "American Airlines",
+    Description =>  "Concierge Key / Executive Platinum"
+  ),
+  array(
+    Name        =>  "United Airlines",
+    Description =>  "1K"
+  )
+);
+?>
+
